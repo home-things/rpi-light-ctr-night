ee48ff8dfb6b05c29094ef29e71b4ef7b4d9ec63
alex <alex@rocketguys.com>
2018-12-27 23:28:44 +0300

    fix

--- debounce.h
+++ debounce.h
@@ -1,1 +1,1 @@
-void debounceImpulse(void (*onImpulse)(void), unsigned int pin, bool *state, bool *lastButtonState, long *lastDebounceTime)
+void debounceImpulse(void (*onImpulse)(void), unsigned int pin, bool_t *state, bool_t *lastButtonState, long *lastDebounceTime)
