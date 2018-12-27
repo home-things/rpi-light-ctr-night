beefdd041c1d7a52c2e9abae0162245ae0edc646
alex <alex@rocketguys.com>
2018-12-27 23:36:56 +0300

    fix

--- debounce.h
+++ debounce.h
@@ -1,1 +1,2 @@
-void debounceImpulse(void (*onImpulse)(void), unsigned int pin, bool_t *state, bool_t *lastButtonState, long *lastDebounceTime)
+#include "isr.h"
+void debounceImpulse(void (*onImpulse)(void), unsigned int pin, bool_t *state, bool_t *lastButtonState, long *lastDebounceTime);
